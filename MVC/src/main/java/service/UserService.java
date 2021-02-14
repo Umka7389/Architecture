2897a3f7188b999c399d12ba00ca9d6c31f34b43
Denis <filippovds89@gmail.com>
2021-02-14 23:48:06 +0300

    HW

---
+++ MVC/src/main/java/service/UserService.java
@@ -0,0 +1,34 @@
+package service;
+
+
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import repository.UserRepository;
+
+
+
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<User> getAllUsers (int page, int size) {
+        return userRepository.findAll(PageRequest.of(page, size));
+    }
+
+
+}
